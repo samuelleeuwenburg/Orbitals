02a8dd1df2e9f97c8a015af62f5affa21a8d9d18
Samuel Leeuwenburg <samleeuwenburg@gmail.com>
2015-03-06 10:00:50 +0100

    add live reload to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var to5 = require('gulp-6to5');
 var concat = require('gulp-concat');
 var order = require('gulp-order');
 var watch = require('gulp-watch');
+var connect = require('gulp-connect');
 
 gulp.task('6to5', function () {
   return gulp.src('src/js/**/*.js')
@@ -14,11 +15,20 @@ gulp.task('6to5', function () {
     ]))
     .pipe(to5())
     .pipe(concat('app.js'))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('build'))
+    .pipe(connect.reload());
+});
+
+gulp.task('webserver', function() {
+  connect.server({
+    root: 'build',
+    livereload: true
+  });
 });
 
 gulp.task('watch', function() {
   gulp.watch('src/js/**/*.js', ['6to5']);
 });
 
+gulp.task('serve', ['webserver', 'watch']);
 
